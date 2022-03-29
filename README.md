900386d94576e26334d1453ad7f249278970d815
Jinchun Choi <noodlejin@gmail.com>
2022-03-29 16:16:34 -0500

    Add statistic results

--- README.md
+++ README.md
@@ -8,9 +8,14 @@
 * Performance and the distribution of search result (time, count).
 * Variance and standard deviation also shows performance result.
 
+
 ## Used package and functions
 ```
 random
 math
 matplotlib
 ```
+
+## References
+- [Python Statistics](https://docs.python.org/3/library/statistics.html#module-statistics)
+- [Matplotlib](https://matplotlib.org/stable/index.html)
--- searching_algorithm_test.py
+++ searching_algorithm_test.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import random
 from datetime import datetime
 import math
-
+import statistics
 
 # Parameters for the experiment
 data_size = 1000
@@ -84,18 +84,36 @@ for i in range(test_count):
 	result_binary, count_binary = binary_search(data, target)
 	binary_count.append(count_binary)
 
-
 print('The Number of Test: ', test_count)
 print('The Size of list (n): ', data_size)
-print('Log(n): ', round(math.log2(data_size), 2))
+print('Log(n): ', round(math.log2(data_size), 2),'\n')
 
 # Calculate count and average of binary search (close to logn) 
 # print(linear_count)
-print('Linear search avg: ',round(average(linear_count), 2))
-
+print('Linear search average: ',round(average(linear_count), 2))
+print('Linear search population variance: ', round(statistics.pvariance(linear_count), 2))
+print('Linear search population standard deviation: ', round(statistics.pstdev(linear_count), 2),'\n')
 
 # Calculate count and average of binary search (close to logn) 
 # print(binary_count)
-print('Binary search avg: ', round(average(binary_count), 2))
+print('Binary search average: ', round(average(binary_count), 2))
+print('Binary search population variance: ', round(statistics.pvariance(binary_count), 2))
+print('Binary search population standard deviation: ', round(statistics.pstdev(binary_count), 2),'\n')
 
+# Draw scatter plot for liner search and binary search
+linear_color = 10
+binary_color = 20
+area =  20
 
+x_axis = [x for x in range(test_count)]
+
+# plt.scatter(x_axis, linear_count, s=area, alpha=1.0)
+# plt.scatter(x_axis, binary_count, s=area, alpha=1.0)
+# plt.show()
+
+# Draw histogram for liner search and binary search
+num_bins = 10
+n, bins, patches = plt.hist(linear_count, num_bins, facecolor='blue', alpha=0.5)
+plt.show()
+n, bins, patches = plt.hist(binary_count, num_bins, facecolor='red', alpha=0.5)
+plt.show()
